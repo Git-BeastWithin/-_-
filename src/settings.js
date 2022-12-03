8655204d2e3af754082c6e791f84cde8a640b4d3
beastwithin <beastwithin0@qq.com>
2022-12-03 21:18:56 +0800

    人资项目初始化

---
+++ src/settings.js
@@ -0,0 +1,17 @@
+// 项目基本信息的配置
+module.exports = {
+  // 项目标题
+  title: 'Vue Admin Template',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: false
+}
